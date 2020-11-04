5d675da20827b94a7c37673a310747e0ed170214
gaya-sid <gayasuddin93@gmail.com>
2020-11-04 22:04:10 +0530

    create and index projects

---
+++ server/app/Controllers/Http/ProjectController.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const Project = use("App/Models/Project");
+
+class ProjectController {
+  // get all projects
+  async index({ auth }) {
+    const user = await auth.getUser();
+    console.log(user);
+    return await user.projects().fetch();
+  }
+
+  async create({ auth, request }) {
+    const user = await auth.getUser();
+    const { title } = request.all();
+
+    const project = new Project();
+
+    project.fill({
+      title,
+    });
+
+    await user.projects().save(project);
+
+    return project;
+  }
+}
+
+module.exports = ProjectController;
